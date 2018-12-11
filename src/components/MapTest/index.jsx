26c6b07e671c093c827256ca4a00c5580cf8a304
Juuso Lehtinen <juuso.lehtinen@codento.com>
2018-12-11 08:57:25 +0200

    Fix map

--- src/components/MapTest/index.jsx
+++ src/components/MapTest/index.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
+import 'leaflet/dist/leaflet.css';
+
 //const { Map: LeafletMap, TileLayer, Marker, Popup } = ReactLeaflet
 
 class MapTest extends Component {
@@ -13,22 +15,23 @@ class MapTest extends Component {
 
   render() {
     const position = [this.state.lat, this.state.lng];
-    return (
-      <Map center={position} zoom={this.state.zoom}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-        />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
 
-      </Map>
+    return (
+      <div className="map-container">
+         <Map center={position} zoom={this.state.zoom}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+          />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </Map>
+      </div>
     )
   }
 }
 
 export default MapTest
-

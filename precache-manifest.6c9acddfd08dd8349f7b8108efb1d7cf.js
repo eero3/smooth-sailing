b20012b86d694c8d38460a39bfec12e4ba8ee948
Juuso Lehtinen <juuso.lehtinen@codento.com>
2018-12-11 13:50:01 +0200

    Updates

---
+++ precache-manifest.6c9acddfd08dd8349f7b8108efb1d7cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3268a617f5aa22429651",
+    "url": "/smooth-sailing/static/js/runtime~main.3268a617.js"
+  },
+  {
+    "revision": "e7dc6b5adc4fab2b4a25",
+    "url": "/smooth-sailing/static/js/main.e7dc6b5a.chunk.js"
+  },
+  {
+    "revision": "dc1a8429bf223ebc47d9",
+    "url": "/smooth-sailing/static/js/1.dc1a8429.chunk.js"
+  },
+  {
+    "revision": "e7dc6b5adc4fab2b4a25",
+    "url": "/smooth-sailing/static/css/main.a5f0f990.chunk.css"
+  },
+  {
+    "revision": "dc1a8429bf223ebc47d9",
+    "url": "/smooth-sailing/static/css/1.3d746e12.chunk.css"
+  },
+  {
+    "revision": "ed492f09968f992531d6ed8c156fb016",
+    "url": "/smooth-sailing/index.html"
+  }
+];
--- src/api/index.js
+++
@@ -1,3 +0,0 @@
-export const reservationStatus = () => fetch('https://aqueous-thicket-34447.herokuapp.com/status')
-export const makeReservation = () => fetch('https://aqueous-thicket-34447.herokuapp.com/add', { method: 'POST' })
-export const cancelReservation = () => fetch('https://aqueous-thicket-34447.herokuapp.com/cancel', { method: 'POST' })
